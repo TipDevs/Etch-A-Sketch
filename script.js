9348dd29f3cfcc3ed250d19e2ba13cda0902031a
TipDevs <tipdevs@gmail.com>
2024-12-21 19:18:43 +0100

    Variable Name Update: I updated the variable from sketchContainer to canvas for simplicity

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Global variable declarations and assignments
-const sketchContainer = document.querySelector('#right-side-square-container'),
+const canvas = document.querySelector('#canvas'),
 inputValue = document.querySelector("#grid-number"),
 createBtn = document.querySelector('#create-btn'),
 resetBtn = document.querySelector('#reset-btn'),
 randomColorBtn = document.querySelector('#random-color-btn'),
 singleColorBtn = document.querySelector('#single-color-btn'),
 leftSideContainer = document.querySelector('#left-side-container'),
-sketchContainerSize = 400;
+canvasSize = 400;
 
 // Add opacity and faded color to sketch container on window load.
-sketchContainer.setAttribute("style", 'opacity: 10%; background-color: #272829; box-shadow: 5px 5px 40px #272829;');
+canvas.setAttribute("style", 'opacity: 10%; background-color: #272829; box-shadow: 5px 5px 40px #272829;');
