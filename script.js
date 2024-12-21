ea19721c985cb414db6f5841ee5b0f7a43414f01
TipDevs <tipdevs@gmail.com>
2024-12-21 12:25:58 +0100

    Global Variables: I create and added global scope variables

---
+++ script.js
@@ -0,0 +1,9 @@
+// Global variable declarations and assignments
+const sketchContainer = document.querySelector('#right-side-square-container'),
+inputValue = document.querySelector("#grid-number"),
+createBtn = document.querySelector('#create-btn'),
+resetBtn = document.querySelector('#reset-btn'),
+randomColorBtn = document.querySelector('#random-color-btn'),
+singleColorBtn = document.querySelector('#single-color-btn'),
+leftSideContainer = document.querySelector('#left-side-container'),
+sketchContainerSize = 400;
