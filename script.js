34bbf89d6c1d16e689860394f9768471b7c6c852
TipDevs <tipdevs@gmail.com>
2024-12-21 23:09:54 +0100

    Variable Declaration: I declared a new variable that stored the canvas initial look

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ randomColorBtn = document.querySelector('#random-color-btn'),
 singleColorBtn = document.querySelector('#single-color-btn'),
 leftSideContainer = document.querySelector('#left-side-container'),
 canvasSize = 400;
+let canvasInitialDisplay = 'opacity: 10%; background-color: #272829; box-shadow: 5px 5px 40px #272829;'
 
 // Add opacity and faded color to sketch container on window load.
 canvas.setAttribute("style", 'opacity: 10%; background-color: #272829; box-shadow: 5px 5px 40px #272829;');
