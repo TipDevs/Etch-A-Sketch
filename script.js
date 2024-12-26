df49f0873df215e91d854981e3ab05e6129ffbd2
TipDevs <tipdevs@gmail.com>
2024-12-26 20:46:50 +0100

    Bug Fixed: I fixed a bug in the activateCanvas function which does not allow the canvas to return fully to initial state if the reset button was clicked

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ canvas.setAttribute('style', canvasInitialDisplay);
 
 // function to run which the square divs base on user input and when create button was clicked.
 function activateCanvas(size) {
-    canvas.setAttribute('style', canvasInitialDisplay = '');
+    canvas.setAttribute('style', '');
     canvas.innerHTML = ''; // Enables the sketch container to be empty initially
     const fragment = document.createDocumentFragment();
     for (i = 0; i < (size * size); i++) {
@@ -27,20 +27,6 @@ function activateCanvas(size) {
     }
     canvas.appendChild(fragment);
 }
-
-/* button that trigger the create square 
-divs function and create square divs in the container
- base on the user input if user input is not less 16 and not greater than 100*/
- inputValue.addEventListener('input', () => {
-    const size = parseInt(inputValue.value);
-    if (size >= 16 && size < 101){
-        activateCanvas(size);
-        console.log(typeof(size));
-    }
-    else {
-        alert("Please input a value from 16 to 100");
-    }
-});
 
 // a function to run that get random color when the random color button was clicked.
 function getRandomColor() {
@@ -79,9 +65,24 @@ addColorsToCanvas();
 function resetCanvas() {
     // button to restore the webpage to initial state.
     resetBtn.addEventListener('click', () => {
-        canvas.removeChild(squareDivs);
+        canvas.innerHTML = '';
         canvas.setAttribute("style", canvasInitialDisplay);
         inputValue.value = '';
+        inputValue.textContent = '';
     });
 }
 resetCanvas();
+
+/* button that trigger the create square 
+divs function and create square divs in the container
+ base on the user input if user input is not less 16 and not greater than 100*/
+ inputValue.addEventListener('input', () => {
+    const size = parseInt(inputValue.value);
+    if (size >= 16 && size < 101){
+        activateCanvas(size);
+        console.log(typeof(size));
+    }
+    else {
+        alert("Please input a value from 16 to 100");
+    }
+});
